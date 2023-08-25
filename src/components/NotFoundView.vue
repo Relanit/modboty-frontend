8b07824a2793ae37286f08348dafdf4710e77c3e
Relanit <stulovmaxim3@gmail.com>
2023-08-25 16:59:22 +0300

    refactoring

---
+++ src/components/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="not-found">
+    <div class="not-found__wrapper">
+      <h1>Ошибка 404: страница не найдена</h1>
+      <img src="img/ppCheck.gif" alt="ppCheck" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.not-found {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 50vh;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AuthViewVue from "../views/AuthView.vue";
+import NotFoundView from "@/components/NotFoundView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,7 +11,7 @@ const routes: Array<RouteRecordRaw> = [
     meta: {
       title: "Главная | ModBoty",
       description:
-        "ModBoty ‒ это модераторский бот для Twitch c возможностью добавления пользовательских команд, управления 7TV смайлами и с многими другими функциями для модерации чата",
+        "ModBoty ‒ это модераторский бот для Twitch c возможностью добавления пользовательских команд, управления 7TV смайлами и функциями для модерации чата",
       keywords: "Twitch, бот, модератор, модерация, 7тв, 7tv, команды, чат",
     },
   },
@@ -33,6 +34,14 @@ const routes: Array<RouteRecordRaw> = [
         "Получение доступа к дополнительным функциям ModBoty, модераторского бота для Twitch",
     },
   },
+  {
+    path: "/:pathMatch(.*)*",
+    component: NotFoundView,
+    meta: {
+      title: "Страница не найдена",
+      description: "",
+    },
+  },
 ];
 
 const router = createRouter({
@@ -42,6 +51,10 @@ const router = createRouter({
 
 router.afterEach((to, from) => {
   document.title = to.meta.title as string;
+  const metaDescription = document.querySelector('meta[name="description"]');
+  if (metaDescription) {
+    metaDescription.setAttribute("content", to.meta.description as string);
+  }
 });
 
 export default router;
